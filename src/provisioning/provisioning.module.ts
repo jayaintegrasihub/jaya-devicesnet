c8e87be1904e1752907c0f0e114fa29f777a7b10
Sigit Nurmajid <sigitnurmajid32@gmail.com>
2024-07-09 11:33:45 +0700

    refactor: provision flow

--- src/provisioning/provisioning.module.ts
+++ src/provisioning/provisioning.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ProvisioningController } from './provisioning.controller';
 import { ProvisioningService } from './provisioning.service';
 import { ApiKeysModule } from 'src/api-keys/api-keys.module';
+import { MqttAclModule } from 'src/mqtt-acl/mqtt-acl.module';
+import { MqttAccountModule } from 'src/mqtt-account/mqtt-account.module';
 
 @Module({
   controllers: [ProvisioningController],
   providers: [ProvisioningService],
-  imports: [ApiKeysModule],
+  imports: [ApiKeysModule, MqttAclModule, MqttAccountModule],
 })
 export class ProvisioningModule {}
