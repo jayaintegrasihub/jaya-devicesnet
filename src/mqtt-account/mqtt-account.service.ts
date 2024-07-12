e6153f014faf759f955da3f8253ac8fc0965b42e
Sigit Nurmajid <sigitnurmajid32@gmail.com>
2024-07-12 14:36:36 +0700

    fix: crypto not defined

--- src/mqtt-account/mqtt-account.service.ts
+++ src/mqtt-account/mqtt-account.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class MqttAccountService {
