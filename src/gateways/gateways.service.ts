6ef7dea7b381183543a541e26d3c83223bfa2dfa
Sigit Nurmajid <sigitnurmajid32@gmail.com>
2024-09-18 09:26:11 +0700

    feat: find nodes on gateway

--- src/gateways/gateways.service.ts
+++ src/gateways/gateways.service.ts
@@ -2,17 +2,28 @@ import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { GatewaysEntity } from './entity/gateways.entity';
 import { REDIS } from 'src/redis/redis.constant';
 import Redis from 'ioredis';
+import { INFLUXDB_CLIENT } from 'src/influxdb/influxdb.constant';
+import { InfluxDB, QueryApi } from '@influxdata/influxdb-client';
+import { ConfigService } from '@nestjs/config';
+import { GatewaysEntity } from './entity/gateways.entity';
 
 @Injectable()
 export class GatewaysService {
+  private queryApi: QueryApi;
+
   constructor(
     private prisma: PrismaService,
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
     @Inject(REDIS) private redis: Redis,
-  ) {}
+    @Inject(INFLUXDB_CLIENT) private influx: InfluxDB,
+    private configService: ConfigService,
+  ) {
+    const org = this.configService.get('INFLUXDB_ORG_ID');
+    const queryApi = this.influx.getQueryApi(org);
+    this.queryApi = queryApi;
+  }
 
   findAll(params: {
     skip?: number;
@@ -29,9 +40,11 @@ export class GatewaysService {
     });
   }
 
-  findOne(where: Prisma.GatewaysWhereUniqueInput) {
+  findOne(identifier: string) {
     return this.prisma.gateways.findFirstOrThrow({
-      where,
+      where: {
+        OR: [{ id: identifier }, { serialNumber: identifier }],
+      },
       include: { tenant: { select: { id: true, name: true } } },
     });
   }
@@ -79,5 +92,43 @@ export class GatewaysService {
       );
       return new GatewaysEntity(gateway);
     }
+  }
+
+  // find node which connected with gateway
+  async findNode(identifier: string) {
+    const { serialNumber, tenant } =
+      await this.prisma.gateways.findFirstOrThrow({
+        where: {
+          OR: [{ id: identifier }, { serialNumber: identifier }],
+        },
+        include: {
+          tenant: {
+            select: { name: true },
+          },
+        },
+      });
+    const fluxQuery = `
+    from(bucket: "${tenant?.name}")
+    |> range(start: -7d)
+    |> filter(fn: (r) => r["_measurement"] == "deviceshealth")
+    |> filter(fn: (r) => r["gateway"] == "${serialNumber}" and r["device"] != "${serialNumber}")
+    |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+    |> last(column : "_time")`;
+    const nodeInflux = await this.queryApi.collectRows(fluxQuery);
+    const listNode = nodeInflux.map((value: any) => {
+      return { serialNumber: value.device };
+    });
+    const nodes = await this.prisma.nodes.findMany({
+      where: {
+        OR: listNode,
+      },
+    });
+    const resultNode = nodes.map((node) => {
+      const nodeFromInflux = nodeInflux.find(
+        (value: any) => value.device === node.serialNumber,
+      ) as any;
+      return { ...node, ...nodeFromInflux };
+    });
+    return resultNode;
   }
 }
