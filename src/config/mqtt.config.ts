12d09739825449e67fb0f0e4e1addfe7aa78758a
Sigit Nurmajid <sigitnurmajid32@gmail.com>
2025-02-26 15:26:25 +0700

    feat: random string client id

--- src/config/mqtt.config.ts
+++ src/config/mqtt.config.ts
@@ -4,5 +4,8 @@ export default registerAs('MQTT', () => ({
   url: process.env.MQTT_URL,
   username: process.env.MQTT_USERNAME,
   password: process.env.MQTT_PASSWORD,
-  clientId: process.env.MQTT_CLIENT_ID,
+  clientId:
+    process.env.MQTT_CLIENT_ID +
+    '_' +
+    Math.random().toString(36).substring(2, 7),
 }));
